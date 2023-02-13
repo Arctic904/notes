4caa2fc4975952cfd1479a762b644de72ff30f9f
Arctic <arcticfire904@gmail.com>
2023-02-13 16:01:18 -0500

    initial testing with paths n shiz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use xdg::BaseDirectories;
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 {
+        println!("The first argument is {}", args[1]);
+    } else {
+        println!("{}", get_current_working_dir().unwrap().to_str().unwrap());
+    }
+}
+
+fn get_current_working_dir() -> std::io::Result<PathBuf> {
+    env::current_dir()
 }
